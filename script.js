54fda680f9e234dc794552f9eb3e24a78b2d9f45
OlegDrakonYakida <drakon.yakida@gmail.com>
2025-08-07 15:36:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let tg = window.Telegram.WebApp;
 let ent = document.getElementById("entb")
 let inp = document.getElementById("NameInput")
 let minp = document.getElementById("MailInput")
+let date = document.getElementById("birthdate")
 
 
 ent.addEventListener("click", () => {
     let data = {
-        name: inp.value,
-        mail: minp.value
+        name: inp.value || "Имя не указано",
+        mail: minp.value || "Почта не указана",
+        date: date.value || "Дата не указана"
     }
 
     tg.sendData(JSON.stringify(data))
