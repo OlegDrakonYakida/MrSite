9e2871e7ba4032c9c073cd0f9bf83bd199fec05b
OlegDrakonYakida <drakon.yakida@gmail.com>
2025-08-13 20:11:27 +0300

    Change

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let tg = window.Telegram.WebApp;
-let sendDataButton = document.getElementById("sendButton")
-let form = document.getElementById("registration")
+const sendDataButton = document.getElementById("sendButton")
+const Phoner = document.getElementById("PhoneInput")
+const tginput = document.getElementById("TgInput")
+const form = document.getElementById("registration")
+const SecRad = document.getElementById("8q")
+const SecInp = document.getElementById("8qInput")
+
+IMask(
+    Phoner,
+    {mask : '+{38} (000)-000-00-00}'}
+);
+
+IMask(
+    tginput,
+    {mask : '{@}************************************************************************'}
+)
+
+SecInp.addEventListener("input", () => {
+    SecRad.value = SecInp.value
+});
 
 sendDataButton.addEventListener("click", (e) => {
     e.preventDefault()
@@ -8,8 +26,16 @@ sendDataButton.addEventListener("click", (e) => {
     let data = {
         name: registrationData.get("UserName") || "Имя не указано",
         date: registrationData.get("UserBirthday") || "Почта не указана",
+        phone: registrationData.get("PhoneInput") || "Номер не указан",
         mail: registrationData.get("UserMail") || "Дата не указана",
-        question: registrationData.get("question") || "Ответ не получен"
+        question: registrationData.get("question") || "Ответ не получен",
+        tgname: registrationData.get("UserTgName") || "Контакт Telegram не указан",
+        question2: registrationData.get("question2") || "Ответ не получен",
+        tq1: registrationData.get("UserQ1") || "Ответ не получен",
+        tg2: registrationData.get("UserQ2") || "Ответ не получен",
+        tg3: registrationData.get("UserQ3") || "Ответ не получен",
+        tg4: registrationData.get("UserQ4") || "Ответ не получен",
+        tg5: registrationData.get("UserQ5") || "Ответ не получен"
     }
 
     tg.sendData(JSON.stringify(data))
