62936d6e62b07ca4b1e178bbd377a0d16887adfa
OlegDrakonYakida <drakon.yakida@gmail.com>
2025-08-29 18:32:23 +0300

    Update web-app

--- script.js
+++ script.js
@@ -20,22 +20,22 @@ SecInp.addEventListener("input", () => {
     SecRad.value = SecInp.value
 });
 
-sendDataButton.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
     let registrationData = new FormData(form)
     let data = {
         name: registrationData.get("UserName") || "Имя не указано",
         date: registrationData.get("UserBirthday") || "Почта не указана",
-        phone: registrationData.get("UserNumberPhone") || "Номер не указан",
+        phone: registrationData.get("PhoneInput") || "Номер не указан",
         mail: registrationData.get("UserMail") || "Дата не указана",
         question: registrationData.get("question") || "Ответ не получен",
         tgname: registrationData.get("UserTgName") || "Контакт Telegram не указан",
         question2: registrationData.get("question2") || "Ответ не получен",
-        tgOne: registrationData.get("UserQ1") || "Ответ не получен",
-        tgTwo: registrationData.get("UserQ2") || "Ответ не получен",
-        tgThree: registrationData.get("UserQ3") || "Ответ не получен",
-        tgFour: registrationData.get("UserQ4") || "Ответ не получен",
-        tgFive: registrationData.get("UserQ5") || "Ответ не получен"
+        tq1: registrationData.get("UserQ1") || "Ответ не получен",
+        tg2: registrationData.get("UserQ2") || "Ответ не получен",
+        tg3: registrationData.get("UserQ3") || "Ответ не получен",
+        tg4: registrationData.get("UserQ4") || "Ответ не получен",
+        tg5: registrationData.get("UserQ5") || "Ответ не получен"
     }
 
     tg.sendData(JSON.stringify(data))
